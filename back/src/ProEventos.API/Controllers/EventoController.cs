e9ae04c240398072d2d12ac2328c3eec189472d5
unknown <rodrigomarquespessoa@gmail.com>
2021-12-13 19:16:03 -0300

    first commit- curso aspnet

---
+++ back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public Evento Get()
+        {
+            return new Evento(){
+                EventoID = 1,
+                Tema = "Asp net"
+            };
+            
+        }
+
+       [HttpPost]
+        public string Post()
+        {
+            return "Exemplo Post";
+            
+        }
+    }
+}
